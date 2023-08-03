e4f24867e66658a812a6a5ca7dac4a2dfc4bbce4
Dimitry Grebenschikov <d.i.grebenschikov@gmail.com>
2023-08-03 15:39:21 +0300

    Выполнить запуск срвера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const serverPort = 8080
+
+func main() {
+
+	port := fmt.Sprintf(":%d", serverPort)
+	// Запуск сервера на порту 8080
+	log.Printf("Серевер запущен на http://localhost%s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
+}

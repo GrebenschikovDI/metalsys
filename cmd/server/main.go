e967adb7635a09f770d611b36744db873927e547
Dimitry Grebenschikov <d.i.grebenschikov@gmail.com>
2023-08-03 17:10:16 +0300

    Выполнить организация хранения метрик

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,45 @@ import (
 
 const serverPort = 8080
 
+type Gauge float64
+
+type Counter int64
+
+// MetricStorage - интерфейс для хранения метрик
+type MetricStorage interface {
+	AddGauge(name string, value Gauge)
+	AddCounter(name string, value Counter)
+}
+
+// MemStorage - реализация MetricStorage на основе map
+type MemStorage struct {
+	gauges   map[string]Gauge
+	counters map[string]Counter
+}
+
+// NewMemStorage - создает новое хранлище метрик
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		gauges:   make(map[string]Gauge),
+		counters: make(map[string]Counter),
+	}
+}
+
+// AddGauge - добавляет значение типа gauge
+func (m *MemStorage) AddGauge(name string, value Gauge) {
+	m.gauges[name] = value
+}
+
+// AddCounter - добавляет значение типа counter
+func (m *MemStorage) AddCounter(name string, value Counter) {
+	current, ok := m.counters[name]
+	if !ok {
+		m.counters[name] = value
+	} else {
+		m.counters[name] = current + value
+	}
+}
+
 func main() {
 
 	port := fmt.Sprintf(":%d", serverPort)

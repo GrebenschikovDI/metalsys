9149aa4ed2442a73bb159c8496f48c65300efcad
Dimitry Grebenschikov <d.i.grebenschikov@gmail.com>
2023-08-05 20:04:43 +0300

    сбор всех метрик из списка

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -9,6 +9,36 @@ import (
 // server - куда будет отправлен запрос
 const server = "http://localhost:8080/"
 
+var metricNames = []string{
+	"Alloc",
+	"BuckHashSys",
+	"Frees",
+	"GCCPUFraction",
+	"GCSys",
+	"HeapAlloc",
+	"HeapIdle",
+	"HeapInuse",
+	"HeapObjects",
+	"HeapReleased",
+	"HeapSys",
+	"LastGC",
+	"Lookups",
+	"MCacheInuse",
+	"MCacheSys",
+	"MSpanInuse",
+	"MSpanSys",
+	"Mallocs",
+	"NextGC",
+	"NumForcedGC",
+	"NumGC",
+	"OtherSys",
+	"PauseTotalNs",
+	"StackInuse",
+	"StackSys",
+	"Sys",
+	"TotalAlloc",
+}
+
 // MetricData - тип данных для сбора и отправки
 type MetricData struct {
 	Name  string
@@ -17,6 +47,7 @@ type MetricData struct {
 }
 
 func main() {
+
 	collectMetrics()
 }
 
@@ -25,11 +56,31 @@ func main() {
 func collectMetrics() []*MetricData {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
-	field := "Alloc"
-	metrics := []*MetricData{
-		{"Alloc", "gauge", float64(reflect.ValueOf(memStats).FieldByName(field).Uint())},
+	var metrics []*MetricData
+	for _, field := range metricNames {
+		value := reflect.ValueOf(memStats).FieldByName(field)
+		var metricValue float64
+
+		switch value.Kind() {
+		case reflect.Uint64:
+			metricValue = float64(value.Uint())
+		case reflect.Uint32:
+			metricValue = float64(value.Uint())
+		case reflect.Float64:
+			metricValue = value.Float()
+		default:
+			fmt.Printf("Неправильный тип метрики %s\n", field)
+			continue
+		}
+
+		metric := &MetricData{
+			Name:  field,
+			Type:  "gauge",
+			Value: metricValue,
+		}
+		fmt.Printf("Name: %s, Type: %s, Value: %f\n", metric.Name, metric.Type, metric.Value)
+		metrics = append(metrics, metric)
 	}
-	fmt.Println(*metrics[0])
 	return metrics
 }
 

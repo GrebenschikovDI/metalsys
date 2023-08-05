028a585f8a7bcd918d5e21302ffb90412dae6559
Dimitry Grebenschikov <d.i.grebenschikov@gmail.com>
2023-08-05 19:09:11 +0300

    Базовый сбор метрики Alloc

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+// server - куда будет отправлен запрос
+const server = "http://localhost:8080/"
+
+// MetricData - тип данных для сбора и отправки
+type MetricData struct {
+	Name  string
+	Type  string
+	Value float64
+}
+
+func main() {
+	collectMetrics()
+}
+
+// сбор метрик
+
+func collectMetrics() []*MetricData {
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+	field := "Alloc"
+	metrics := []*MetricData{
+		{"Alloc", "gauge", float64(reflect.ValueOf(memStats).FieldByName(field).Uint())},
+	}
+	fmt.Println(*metrics[0])
+	return metrics
+}
+
+// подготовка метрик
+// отправка метрик

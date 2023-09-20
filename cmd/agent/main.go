78c6ae8cf5e01c5fdfd86e8b5affdf86198aaf72
Dimitry Grebenschikov <d.i.grebenschikov@gmail.com>
2023-09-20 21:13:19 +0300

    fix: light clean up

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,36 +11,6 @@ import (
 	"time"
 )
 
-var metricNames = []string{
-	"Alloc",
-	"BuckHashSys",
-	"Frees",
-	"GCCPUFraction",
-	"GCSys",
-	"HeapAlloc",
-	"HeapIdle",
-	"HeapInuse",
-	"HeapObjects",
-	"HeapReleased",
-	"HeapSys",
-	"LastGC",
-	"Lookups",
-	"MCacheInuse",
-	"MCacheSys",
-	"MSpanInuse",
-	"MSpanSys",
-	"Mallocs",
-	"NextGC",
-	"NumForcedGC",
-	"NumGC",
-	"OtherSys",
-	"PauseTotalNs",
-	"StackInuse",
-	"StackSys",
-	"Sys",
-	"TotalAlloc",
-}
-
 func main() {
 	cfg, err := config.LoadConfig()
 	if err != nil {
